c4a88de0f588fd5700e282880fc05eaf760d6c12
Jeff Lill <jeff@lilltek.com>
2021-04-28 10:54:46 -0700

    coding...

--- action.ps1
+++ action.ps1
@@ -293,7 +293,7 @@ try
                 $resultSummaries += ";"
             }
 
-            $resultUris += "[$projectName](https://github.com/nforgeio/test-results/blob/master/results/$timestamp-$projectName.md)"
+            $resultUris += "[results](https://github.com/nforgeio/test-results/blob/master/results/$timestamp-$projectName.md)"
 
             # $hack(jefflill):
             #
